87d1a7babccc7e1a0a2c4ca2d051bec4c5c05a81
PC <PC@Pooja-1Bhosale>
2022-07-19 13:06:11 +0530

    [Puja1] Hey Team Plz. acess new1

---
+++ src/A/demo1.java
@@ -0,0 +1,20 @@
+package A;
+
+public class demo1 
+{
+	//URL git: https://github.com/bhosale155/New1.git
+	//Token:
+	//UN   : bhosale155
+	//pwd  : Niphad@2019
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hii2");
+		System.out.println("Hello");
+		System.out.println("Good Morning");
+		System.out.println("Nice1");
+		
+	}
+
+
+}
